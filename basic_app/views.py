df27c68ee69fc332b234e5db0055334edcc53cdc
Akmal Makhmudov <48078221+AkmalMakh@users.noreply.github.com>
2021-07-16 22:22:32 +0900

    working with clased based views

--- basic_app/views.py
+++ basic_app/views.py
@@ -1,13 +1,20 @@
-from django.http.response import HttpResponse
+from django.db import models
 from django.shortcuts import render
-from django.views.generic import View, TemplateView
+from django.views.generic import View, TemplateView, ListView, DetailView
+from . import models
 
 class IndexView(TemplateView):
     template_name = 'index.html'
 
+class SchoolListView(ListView):
+    # defining conetxt object name by default it is school_list because of we are using Listview
+    context_object_name = 'schools'
+    # returns all the list from school model
+    model = models.School
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['injectme']='BASIC INJECTION!'
-        return context
+class SchoolDetailView(DetailView):
+     # defining conetxt object name by default it is school_list because of we are using Listview
+    context_object_name = 'school_details'
+    model = models.School
+    template_name = 'basic_app/school_detail.html'
 
